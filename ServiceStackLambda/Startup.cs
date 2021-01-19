42392b1db45cbdc8d5f301442e3d67a916badd1a
Darren Reid <darren@reidmail.org>
2021-01-19 11:40:27 +1100

    Initial commit

---
+++ ServiceStackLambda/Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Funq;
+using Microsoft.AspNetCore.Http;
+using ServiceStack;
+using ServiceStack.Configuration;
+using ServiceStackLambda.ServiceInterface;
+
+namespace ServiceStackLambda
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(next => context =>
+            {
+                context.Request.EnableBuffering();
+                return next(context);
+            });
+
+            app.UseServiceStack(new AppHost
+            {
+                AppSettings = new AppSettings(),
+            });
+        }
+    }
+
+    public class AppHost : AppHostBase
+    {
+        public AppHost() : base("ServiceStackLambda", typeof(MyServices).Assembly) { }
+
+        // Configure your AppHost with the necessary configuration and dependencies your App needs
+        public override void Configure(Container container)
+        {
+            SetConfig(new HostConfig
+            {
+                DebugMode = AppSettings.Get(nameof(HostConfig.DebugMode), false)
+            });
+        }
+    }
+}
